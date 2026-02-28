521a104245db54aafa7449def172bc5523e0b7e6
harun-09 <md.harun.sk09@gmail.com>
2026-02-28 07:08:51 +0600

    fix(api): expose only required assessment endpoints

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\ApplicationController;
 
-Route::apiResource('jobs', JobController::class);
-Route::apiResource('applications', ApplicationController::class);
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/{id}', [JobController::class, 'show']);
+Route::post('/jobs', [JobController::class, 'store']);
+Route::delete('/jobs/{id}', [JobController::class, 'destroy']);
+
+Route::post('/applications', [ApplicationController::class, 'store']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
